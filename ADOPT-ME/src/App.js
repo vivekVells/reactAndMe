06e410416754baed3bd4c4ece57b42a99cd453ac
Vivek Vellaiyappan Surulimuthu <vsurulimuthu@athenahealth.com>
2020-01-08 13:54:41 -0600

    reusable components by using props

--- ADOPT-ME/src/App.js
+++ ADOPT-ME/src/App.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
     return React.createElement("div", {}, [ 
-        React.createElement("h1", {}, "Scooby"),
-        React.createElement("h2", {}, "Dog"),
-        React.createElement("h2", {}, "Pomeranian"),
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
     ]);
 }
 
@@ -10,7 +10,9 @@ const App = () => {
     return React.createElement(
         "div", {},
         React.createElement("h1", {}, "Adopt me"),
-        React.createElement(Pet)
+        React.createElement(Pet, {name: "Brownie", animal: "Dog", breed: "Pomeranian"}),
+        React.createElement(Pet, {name: "Tom", animal: "Cat", breed: "Mixed"}),
+        React.createElement(Pet, {name: "Jerry", animal: "Mouse", breed: "Rodent"})
     );
 }
 
