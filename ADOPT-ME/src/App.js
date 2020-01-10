8d0c70541fe52d051c4c4e58b94314b4f29dd0d4
Vivek Vellaiyappan Surulimuthu <vsurulimuthu@athenahealth.com>
2020-01-10 14:38:21 -0600

    updated prettier config. updated npm script for using prettier formatting

--- ADOPT-ME/src/App.js
+++ ADOPT-ME/src/App.js
@@ -2,21 +2,27 @@ const Pet = ({ name, animal, breed }) => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, name),
         React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
+        React.createElement("h2", {}, breed)
     ]);
-}
+};
 
 const App = () => {
     return React.createElement(
-        "div", {},
+        "div",
+        {},
         React.createElement("h1", {}, "Adopt me"),
-        React.createElement(Pet, { name: "Brownie", animal: "Dog", breed: "Pomeranian" }),
+        React.createElement(Pet, {
+            name: "Brownie",
+            animal: "Dog",
+            breed: "Pomeranian"
+        }),
         React.createElement(Pet, { name: "Tom", animal: "Cat", breed: "Mixed" }),
-        React.createElement(Pet, { name: "Jerry", animal: "Mouse", breed: "Rodent" })
+        React.createElement(Pet, {
+            name: "Jerry",
+            animal: "Mouse",
+            breed: "Rodent"
+        })
     );
-}
+};
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
